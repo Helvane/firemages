b2b9fa46c5f64c828a7c8d556e8bb483ff4f2a09
sam <sam@sams-MacBook-Pro.local>
2015-01-18 22:53:58 -0500

    fix endpoint

--- config/endpoint.js
+++ config/endpoint.js
@@ -2,10 +2,10 @@
  * Created by king on 12/27/14.
  */
 
-var REGISTERURL='http://localhost:1337/Users/create';
-var REGISTERURLUPDATE='http://localhost:1337/Users/update';
-var USERSURL='http://localhost:1337/Users/getusers';
-var LOGINURL='http://localhost:1337/Users/login';
+var REGISTERURL='http://www.firemages.com:1337/Users/create';
+var REGISTERURLUPDATE='http://www.firemages.com:1337/Users/update';
+var USERSURL='http://www.firemages.com:1337/Users/getusers';
+var LOGINURL='http://www.firemages.com:1337/Users/login';
 
-var BLOGURL='http://localhost:1337/Blog/create';
-var GETBLOGURL='http://localhost:1337/Blog/getblog';
+var BLOGURL='http://www.firemages.com:1337/Blog/create';
+var GETBLOGURL='http://www.firemages.com:1337/Blog/getblog';
